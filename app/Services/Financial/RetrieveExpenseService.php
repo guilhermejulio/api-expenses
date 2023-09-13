bf2545d888b7097bbbd36456dd2fd50eead247c2
Guilherme Julio <guilhermejuliocode@gmail.com>
2023-09-12 21:34:40 -0300

    feat: Retrieve expenses service

---
+++ app/Services/Financial/RetrieveExpenseService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\Financial;
+
+use App\Http\Resources\Financial\UserExpensesResource;
+use App\Repositories\Contracts\Financial\RetrieveExpenseInterface;
+
+class RetrieveExpenseService
+{
+    private RetrieveExpenseInterface $retrieveExpenseRepository;
+
+    public function __construct(RetrieveExpenseInterface $retrieveExpenseRepository)
+    {
+        $this->retrieveExpenseRepository = $retrieveExpenseRepository;
+    }
+
+    public function retrieveExpensesFromUser(int $userId): UserExpensesResource
+    {
+        return $this->retrieveExpenseRepository->retrieveExpensesFromUser($userId);
+    }
+}
