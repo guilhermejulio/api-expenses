0fb51ff6bae411998054f17a5ceb03fb76b04a86
Guilherme Julio <guilhermejuliocode@gmail.com>
2023-09-15 10:08:49 -0300

    Send email for new transaction

--- app/Services/Financial/PersistExpenseService.php
+++ app/Services/Financial/PersistExpenseService.php
@@ -4,7 +4,10 @@ namespace App\Services\Financial;
 
 use App\Dto\Financial\CreateExpenseDTO;
 use App\Http\Resources\Financial\ExpenseResource;
+use App\Notifications\NewExpenseEmailNotification;
 use App\Repositories\Contracts\Financial\PersistExpenseInterface;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class PersistExpenseService
 {
@@ -17,6 +20,21 @@ class PersistExpenseService
 
     public function persist(CreateExpenseDTO $expenseDTO): ExpenseResource
     {
-        return $this->persistRepository->persistExpense($expenseDTO);
+        $resource = $this->persistRepository->persistExpense($expenseDTO);
+        try {
+            $this->sendNotification($expenseDTO->getDescription(), $expenseDTO->getAmount());
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+        return new ExpenseResource($resource);
+    }
+
+    private function sendNotification(string $description, string $amount): void
+    {
+        $user = Auth::user();
+        $user->description = $description;
+        $user->amount = $amount;
+        $user->notify((new NewExpenseEmailNotification())->onQueue('email'));
     }
 }
